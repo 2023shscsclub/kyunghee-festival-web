bccecba061720541df95069e4d18feb43e665f5a
hyunwoo6321 <phw1508@naver.com>
2023-12-28 05:16:11 +0900

    home page half complete

--- frontend/src/pages/Home.tsx
+++ frontend/src/pages/Home.tsx
@@ -5,22 +5,22 @@ export default function Home() {
         <div className={"w-full relative z-10"}>
             <div className={"w-full bg-main flex flex-col justify-center items-center text-white"}>
                 <div className={"w-full flex flex-col items-center bg-black"}>
-                    <img src={"./vr.png"} alt={"vr"} className={"w-60 mx-auto rounded-2xl mt-6"}/>
+                    <img src={"./vr.png"} alt={"vr"} className={"w-60 mx-auto rounded-2xl my-6"}/>
                     <h1 className={"text-3xl font-extrabold w-60"}>VR체험</h1>
                     <h1 className={"w-60 break-words mb-6"}>vdsadoijdodjasjdijidoskmjiokmoidsolijlkmjklmasdjmaidjmsodjmoiadj</h1>
                 </div>
                 <div className={"w-full flex flex-col items-center"}>
-                    <img src={"./tiktaktoe.png"} alt={"vr"} className={"w-60 mx-auto rounded-2xl mt-6"}/>
+                    <img src={"./tiktaktoe.png"} alt={"vr"} className={"w-60 mx-auto rounded-2xl my-6"}/>
                     <h1 className={"text-3xl font-extrabold w-60"}>AI VS 인간</h1>
                     <h1 className={"w-60 break-words mb-6"}>vdsadoijdodjasjdijidoskmjiokmoidsolijlkmjklmasdjmaidjmsodjmoiadj</h1>
                 </div>
                 <div className={"w-full flex flex-col items-center bg-black"}>
-                    <img src={"./cafe.png"} alt={"vr"} className={"w-60 mx-auto rounded-2xl mt-6"}/>
+                    <img src={"./cafe.png"} alt={"vr"} className={"w-60 mx-auto rounded-2xl my-6"}/>
                     <h1 className={"text-3xl font-extrabold w-60"}>로봇팔 카페</h1>
                     <h1 className={"w-60 break-words mb-6"}>vdsadoijdodjasjdijidoskmjiokmoidsolijlkmjklmasdjmaidjmsodjmoiadj</h1>
                 </div>
                 <div className={"w-full flex flex-col items-center"}>
-                    <img src={"./cafe.png"} alt={"vr"} className={"w-60 mx-auto rounded-2xl mt-6"}/>
+                    <img src={"./cafe.png"} alt={"vr"} className={"w-60 mx-auto rounded-2xl my-6"}/>
                     <h1 className={"text-3xl font-extrabold w-60"}>해킹 소개</h1>
                     <h1 className={"w-60 break-words mb-6"}>vdsadoijdodjasjdijidoskmjiokmoidsolijlkmjklmasdjmaidjmsodjmoiadj</h1>
                 </div>
