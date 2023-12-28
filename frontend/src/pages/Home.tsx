4e637edea66eb4b435df67ae05dd630ff1a8a532
Hyunwoo Park <65493966+MBP16@users.noreply.github.com>
2023-12-28 12:38:25 +0900

    Update Home.tsx

--- frontend/src/pages/Home.tsx
+++ frontend/src/pages/Home.tsx
@@ -6,15 +6,15 @@ export default function Home() {
             <div className={"w-full bg-main flex flex-col justify-center items-center text-white"}>
                 <div className={"w-full flex flex-col items-center bg-black"}>
                     <img src={"./vr.png"} alt={"vr"} className={"w-60 mx-auto rounded-2xl my-6"}/>
-                    <h1 className={"text-3xl font-extrabold w-60"}>VR체험</h1>
+                    <h1 className={"text-3xl font-extrabold w-60 mb-6"}>VR체험</h1>
                 </div>
                 <div className={"w-full flex flex-col items-center"}>
                     <img src={"./tiktaktoe.png"} alt={"vr"} className={"w-60 mx-auto rounded-2xl my-6"}/>
-                    <h1 className={"text-3xl font-extrabold w-60"}>AI VS 인간</h1>
+                    <h1 className={"text-3xl font-extrabold w-60 mb-6"}>AI VS 인간</h1>
                 </div>
                 <div className={"w-full flex flex-col items-center bg-black"}>
                     <img src={"./cafe.png"} alt={"vr"} className={"w-60 mx-auto rounded-2xl my-6"}/>
-                    <h1 className={"text-3xl font-extrabold w-60"}>로봇팔 카페</h1>
+                    <h1 className={"text-3xl font-extrabold w-60 mb-6"}>로봇팔 카페</h1>
                 </div>
             </div>
         </div>
