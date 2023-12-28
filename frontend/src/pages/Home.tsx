1ed3069fbd624731eaa9e2f40f7ff253b93b8459
Hyunwoo Park <65493966+MBP16@users.noreply.github.com>
2023-12-28 12:34:12 +0900

    Update Home.tsx

--- frontend/src/pages/Home.tsx
+++ frontend/src/pages/Home.tsx
@@ -1,7 +1,7 @@
 import React from "react";
 
 export default function Home() {
-    return (aaq
+    return
         <div className={"w-full relative z-10"}>
             <div className={"w-full bg-main flex flex-col justify-center items-center text-white"}>
                 <div className={"w-full flex flex-col items-center bg-black"}>
@@ -12,7 +12,7 @@ export default function Home() {
                     <img src={"./tiktaktoe.png"} alt={"vr"} className={"w-60 mx-auto rounded-2xl my-6"}/>
                     <h1 className={"text-3xl font-extrabold w-60"}>AI VS 인간</h1>
                 </div>
-Q1¹1q                <div className={"w-full flex flex-col items-center bg-black"}>
+                <div className={"w-full flex flex-col items-center bg-black"}>
                     <img src={"./cafe.png"} alt={"vr"} className={"w-60 mx-auto rounded-2xl my-6"}/>
                     <h1 className={"text-3xl font-extrabold w-60"}>로봇팔 카페</h1>
                 </div>
