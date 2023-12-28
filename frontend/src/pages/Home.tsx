f16ad467699783d9a8d51370569a14a830747f31
Hyunwoo Park <65493966+MBP16@users.noreply.github.com>
2023-12-28 12:28:46 +0900

    Home.tsx 업데이트

--- frontend/src/pages/Home.tsx
+++ frontend/src/pages/Home.tsx
@@ -1,28 +1,20 @@
 import React from "react";
 
 export default function Home() {
-    return (
+    return (aaq
         <div className={"w-full relative z-10"}>
             <div className={"w-full bg-main flex flex-col justify-center items-center text-white"}>
                 <div className={"w-full flex flex-col items-center bg-black"}>
                     <img src={"./vr.png"} alt={"vr"} className={"w-60 mx-auto rounded-2xl my-6"}/>
                     <h1 className={"text-3xl font-extrabold w-60"}>VR체험</h1>
-                    <h1 className={"w-60 break-words mb-6"}>vdsadoijdodjasjdijidoskmjiokmoidsolijlkmjklmasdjmaidjmsodjmoiadj</h1>
                 </div>
                 <div className={"w-full flex flex-col items-center"}>
                     <img src={"./tiktaktoe.png"} alt={"vr"} className={"w-60 mx-auto rounded-2xl my-6"}/>
                     <h1 className={"text-3xl font-extrabold w-60"}>AI VS 인간</h1>
-                    <h1 className={"w-60 break-words mb-6"}>vdsadoijdodjasjdijidoskmjiokmoidsolijlkmjklmasdjmaidjmsodjmoiadj</h1>
                 </div>
-                <div className={"w-full flex flex-col items-center bg-black"}>
+Q1¹1q                <div className={"w-full flex flex-col items-center bg-black"}>
                     <img src={"./cafe.png"} alt={"vr"} className={"w-60 mx-auto rounded-2xl my-6"}/>
                     <h1 className={"text-3xl font-extrabold w-60"}>로봇팔 카페</h1>
-                    <h1 className={"w-60 break-words mb-6"}>vdsadoijdodjasjdijidoskmjiokmoidsolijlkmjklmasdjmaidjmsodjmoiadj</h1>
-                </div>
-                <div className={"w-full flex flex-col items-center"}>
-                    <img src={"./cafe.png"} alt={"vr"} className={"w-60 mx-auto rounded-2xl my-6"}/>
-                    <h1 className={"text-3xl font-extrabold w-60"}>해킹 소개</h1>
-                    <h1 className={"w-60 break-words mb-6"}>vdsadoijdodjasjdijidoskmjiokmoidsolijlkmjklmasdjmaidjmsodjmoiadj</h1>
                 </div>
             </div>
         </div>
